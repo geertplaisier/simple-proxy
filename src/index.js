357f396d2f70c2e2a91ac6799d7067a70b9f3bc4
Geert Plaisier <geert.plaisier@gnamic.nl>
2021-11-24 10:18:11 +0100

    Updated package & added signalR

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
 
-app.use('/', createProxyMiddleware({
-	target: '[REMOTE_URL_HERE]',
+
+const cookie = '[COOKIE_CONTENT_HERE]';
+const target = '[REMOTE_URL_HERE]';
+
+app.use('/signalr', createProxyMiddleware({
+	target: target,
+	ws: true,
 	changeOrigin: true,
 	headers: {
-		'Cookie': '[COOKIE_CONTENT_HERE]'
+		'Cookie': cookie.replace('Cookie: ', '')
 	},
 	secure: false,
 	logLevel: 'debug'
 }));
+
+app.use('/', createProxyMiddleware({
+	target: target,
+	changeOrigin: true,
+	headers: {
+		'Cookie': cookie.replace('Cookie: ', '')
+	},
+	secure: false,
+	logLevel: 'debug'
+}));
+
 app.listen(3000);
